4e1473b8e38ed04a84524e3b1ce2b17144d6cda2
Marta Fabjanska <marta.fabjanska@onegini.com>
2019-10-27 22:27:14 +0100

    run simple test

--- src/test/java/com/onegini/integration/demo/testcontainers/LoginControllerIT.java
+++ src/test/java/com/onegini/integration/demo/testcontainers/LoginControllerIT.java
@@ -1,6 +1,7 @@
 package com.onegini.integration.demo.testcontainers;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 
@@ -8,9 +9,11 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -21,27 +24,29 @@ public class LoginControllerIT {
 
   private static final int SERVICE_SERVER_PORT = 8080;
 
-  @ClassRule
-  public static DockerComposeContainer DOCKER_COMPOSE_CONTAINER = new DockerComposeContainer(new File("src/test/resources/docker-service-demo.yml"))
-      .withExposedService("simple-service", SERVICE_SERVER_PORT, Wait.forListeningPort());
+//  @ClassRule
+//  public static DockerComposeContainer DOCKER_COMPOSE_CONTAINER = new DockerComposeContainer(new File("src/test/resources/docker-service-demo.yml"))
+//      .withExposedService("simple-service", SERVICE_SERVER_PORT, Wait.forListeningPort());
 
-  /*public static GenericContainer DOCKER_COMPOSE_CONTAINER =
-      new GenericContainer("martaonegini/simple-service-demo:8")
-          .withExposedPorts(SERVICE_SERVER_PORT);*/
+  @Rule
+  public MariaDBContainer mariaDB = new MariaDBContainer();
+
 
   @Test
   public void testLogin() {
-    DOCKER_COMPOSE_CONTAINER.start();
 
-    given()
-        .port(SERVICE_SERVER_PORT)
-        .contentType(ContentType.JSON)
-        .accept(ContentType.JSON)
-        .body("{\"login\":\"marta\",\"password\":\"test\"}")
-        .log().uri()
-    .when()
-        .post("login")
-    .then()
-        .body("welcome", Matchers.is("Welcome marta"));
+    assertTrue(true);
+//    DOCKER_COMPOSE_CONTAINER.start();
+//
+//    given()
+//        .port(SERVICE_SERVER_PORT)
+//        .contentType(ContentType.JSON)
+//        .accept(ContentType.JSON)
+//        .body("{\"login\":\"marta\",\"password\":\"test\"}")
+//        .log().uri()
+//    .when()
+//        .post("login")
+//    .then()
+//        .body("welcome", Matchers.is("Welcome marta"));
   }
 }
